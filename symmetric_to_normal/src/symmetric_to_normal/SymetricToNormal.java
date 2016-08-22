84ff787d4327f67732e91649b992600d7272bb1b
Suad <suadbasbug@gmail.com>
2016-08-22 12:16:05 -0700

    Some new values

--- symmetric_to_normal/src/symmetric_to_normal/SymetricToNormal.java
+++ symmetric_to_normal/src/symmetric_to_normal/SymetricToNormal.java
@@ -4,7 +4,8 @@ public class SymetricToNormal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double[] symmetric = {0.13125, 0.77541, 0.99704, 1.65704, 1.85234, 2.57524, 2.65103, 3.35482, 3.52757, 4.41858};
+		//double[] symmetric = {0.13125, 0.77541, 0.99704, 1.65704, 1.85234, 2.57524, 2.65103, 3.35482, 3.52757, 4.41858};
+		double[] symmetric = {0.25, 0.75, 1.25, 1.75, 2.25, 2.75, 3.25};
 		
 		double[] normal = new double[symmetric.length*2];
 		normal[0] = 0;
